207329a56047354dfa631f67998d378c7d3f1052
mah454 <mah454@gmail.com>
2026-07-24 16:41:42 +0330

    fix

--- src/main/groovy/com/sample/HelloJob.groovy
+++ src/main/groovy/com/sample/HelloJob.groovy
@@ -5,6 +5,6 @@ import ir.moke.microfox.api.job.Task
 class HelloJob implements Task {
     @Override
     void run() {
-        println "Job V2 Executed"
+        println "Job V0.1 Executed"
     }
 }
