f456a9d9e77ad7017f8c68247c3de1596da87469
mah454 <mah454@gmail.com>
2026-07-27 23:49:46 +0330

    Improvement log

--- src/main/groovy/com/sample/HelloJob.groovy
+++ src/main/groovy/com/sample/HelloJob.groovy
@@ -2,11 +2,15 @@ package com.sample
 
 import com.sample.service.PersonService
 import ir.moke.orchestra.api.Task
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class HelloJob implements Task {
+    private static final Logger logger = LoggerFactory.getLogger(HelloJob)
+
     @Override
     void run() {
-        println "Job V0.1 Executed"
+        logger.info("Job V0.2 Executed")
         PersonService.savePerson()
     }
 }
--- src/main/groovy/com/sample/service/PersonService.groovy
+++ src/main/groovy/com/sample/service/PersonService.groovy
@@ -2,8 +2,11 @@ package com.sample.service
 
 import com.sample.entity.Person
 import ir.moke.orchestra.api.persistance.OptionalRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class PersonService {
+    private static final Logger logger = LoggerFactory.getLogger(PersonService)
     private static final OptionalRepository<Person> repo = OptionalRepository.of(Person)
 
     static def savePerson() {
@@ -11,5 +14,6 @@ class PersonService {
         p.name = "Mahdi"
         p.family = "Sheikh Hosseini"
         repo.save(p)
+        logger.info("Save Person")
     }
 }
